c683584a1bb2e69a0008e3e41c2cb06a240a156f
proeszti <kispothajas@gmail.com>
2021-11-17 09:10:56 +0100

    finished

--- solutions/app2.js
+++ solutions/app2.js
@@ -5,15 +5,15 @@
 // Írj egy függvényt, amely paraméterként szintén egy tömböt kap. A függvény visszatérési értéke egy Promise.
 // A Promise resolved lesz, amennyiben a tömb minden eleme string, és visszatér ezek abc-rend szerint rendezett tömbjével.
 // Ha nem minden elem string, akkor a Promise rejected lesz, visszatérési értéke: Error: Not all elements are string type!
-const sorterPromise = (arr) => new Promise((resolve, reject) {
+const sorterPromise = (arr = []) => new Promise((resolve, reject) => {
     if (arr.every(item => typeof item === "string")) {
         resolve(arr.sort())
     } else {
         reject("Error: Not all elements are string type!")
     }
-   
-)
 }
+
+)
 
 
 
